bce6be884980a97bf768db8e6552dd243831da4d
Александр <76145069+NightmareUnderpants@users.noreply.github.com>
2025-07-13 14:40:19 +1000

    Update PropInteract.cs
    update to more useful example

--- Scripts/Interact System/Examples/Interact Prop/PropInteract.cs
+++ Scripts/Interact System/Examples/Interact Prop/PropInteract.cs
@@ -1,38 +1,19 @@
 using System;
 using UnityEngine;
 
-public class PropInteract : MonoBehaviour, IInteractable
+public class ExampleInteract : MonoBehaviour, IInteractable
 {
-    [Header("Unavailiable Interaction")]
     [SerializeField]
-    private bool isInteractable;
-    [SerializeField]
-    private string UnInteractiveText;
-
-    [Header("Optional: Add height to InteractIcon transform")]
-    [SerializeField]
-    private float addHeight;
-
-    public static event Action<string> OnInteract;
+    private bool _isInteractable;
 
     public void Interact(Transform interactorTransform)
     {
-        OnInteract?.Invoke(gameObject.name);
+        // interact logic
     }
 
     public void ChangeInteractState(bool state)
     {
-        isInteractable = state;
-    }
-
-    public string GetInteractText()
-    {
-        return "Interact with Object";
-    }
-
-    public Transform GetInteractTransform()
-    {
-        return transform;
+        _isInteractable = state;
     }
 
     public Vector3 GetInteractVector3()
@@ -40,13 +21,11 @@ public class PropInteract : MonoBehaviour, IInteractable
         return new Vector3(transform.position.x, transform.position.y + addHeight, transform.position.z);
     }
 
-    public string UnavailableIntegration()
-    {
-        return UnInteractiveText;
-    }
-
     public bool IsInteractable()
     {
-        return isInteractable;
+        return _isInteractable;
     }
 }
+
+
+
